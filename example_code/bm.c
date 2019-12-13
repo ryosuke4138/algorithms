74e08eb64dc1a4718421d59f4d246fd7094290ca
Ryosuke Okamae <okamae.ryosuke@unipro.jp>
2019-12-13 10:20:20 +0900

    挿入したところ覚えておいて、挿入したところは飛ばす

--- example_code/bm.c
+++ example_code/bm.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include<string.h>
+#include <string.h>
 
 #define LIM_LEN 15
 
@@ -35,15 +35,16 @@ void create_table(S s, int *table){
 }
 
 //Tのpointからsに置き換える
-void insert(char *T, S s, int point){
+void insert(char *T, char *inserted, S s, int point){
     int i;
     for(i=0;i<s.w_len;i++){
         T[point+i]=s.word[i];
+        inserted[point+i]='y';
     }
 }
 
 //bm法
-void bm(char *T, int t_len, S s){
+int bm(char *T, char *inserted, int t_len, S s){
     int table[4];
     create_table(s,table);
     int pt=s.w_len-1;
@@ -52,50 +53,55 @@ void bm(char *T, int t_len, S s){
     while (pt<t_len)
     {
         ps=s.w_len-1;
-        while (T[pt]==s.word[ps]||T[pt]=='x')
+        while ((T[pt]==s.word[ps]||T[pt]=='x')&&inserted[pt]=='n')
         {
             if(ps==0){
-                insert(T,s,pt);
-                return;
+                return pt;
             }
             pt--;
             ps--;
         }
-        word=T[pt]-97;
-        pt+=(table[word]>s.w_len-ps) ? table[word] : s.w_len-ps;
+        if(inserted[pt]=='y'){
+            pt+=s.w_len;
+        }
+        else{
+            word=T[pt]-97;
+            pt+=(table[word]>s.w_len-ps) ? table[word] : s.w_len-ps;
+        }
     }
-    return;
+    return -1;
 }
 
 int main_prg(int, char**);
 
 int main(int argc, char** argv){
 
-  struct rusage u;
-  struct timeval start, end;
+    struct rusage u;
+    struct timeval start, end;
   
-  getrusage(RUSAGE_SELF, &u);
-  start = u.ru_utime;
+    getrusage(RUSAGE_SELF, &u);
+    start = u.ru_utime;
   
-  main_prg(argc, argv);
+    main_prg(argc, argv);
   
-  getrusage(RUSAGE_SELF, &u );
-  end = u.ru_utime;
+    getrusage(RUSAGE_SELF, &u );
+    end = u.ru_utime;
   
-  fprintf(stderr, "%lf\n", (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec)*1e-6);
+    fprintf(stderr, "%lf\n", (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec)*1e-6);
     
-  return(0);
+    return(0);
 }
 
 int main_prg(int argc, char** argv){
-   assert( argc==3 );
-   FILE *fp_in = fopen( argv[1], "r" );
-   assert( fp_in != NULL );
-   FILE *fp_out = fopen( argv[2], "w" );
-   assert( fp_out != NULL );
+    assert( argc==3 );
+    FILE *fp_in = fopen( argv[1], "r" );
+    assert( fp_in != NULL );
+    FILE *fp_out = fopen( argv[2], "w" );
+    assert( fp_out != NULL );
+    int i;
 
     //T'の用意
-    char *T_dash=(char*)malloc(sizeof(char)*500000);
+    char *T_dash=(char*)malloc(sizeof(char)*450000);
     fscanf(fp_in,"%s\n",T_dash);
     int t_len=strlen(T_dash);
 
@@ -103,12 +109,18 @@ int main_prg(int argc, char** argv){
     char *T_in=(char*)malloc(sizeof(char)*500000);
     strcpy(T_in, T_dash);
 
+    //挿入された場所かどうか
+    char inserted[t_len];
+    for(i=0;i<t_len;i++){
+        inserted[i]='n';
+    }
+
     //Sの用意
-    S *s = malloc(50000 * sizeof(S)); // S s[50000];
+    S *s = malloc(50000 * sizeof(S));
     int s_cnt=0;
     while(fgets(s[s_cnt].word,150,fp_in)!=NULL){
         s[s_cnt].w_len=strlen(s[s_cnt].word)-1;
-        if(s[s_cnt].w_len>1){
+        if(s[s_cnt].w_len>=LIM_LEN){
             if(s[s_cnt].word[s[s_cnt].w_len]=='\n') s[s_cnt].word[s[s_cnt].w_len]='\0';
             s_cnt++;
         }
@@ -121,12 +133,10 @@ int main_prg(int argc, char** argv){
     qsort(s,s_cnt,sizeof(S),(int (*)(const void *, const void *))comp);
 
     //bm法で挿入
-    int i;
+    int a;
     for(i=0;i<s_cnt;i++){
-        //長さ15未満の断片は挿入しない
-        if(s[i].w_len < LIM_LEN) break;
-
-        bm(T_dash,t_len,s[i]);
+        a=bm(T_dash,inserted,t_len,s[i]);
+        if(a>=0) insert(T_dash,inserted,s[i],a);
     }
 
     //置き換えられなかったところをaに置き換える
@@ -144,10 +154,9 @@ int main_prg(int argc, char** argv){
 
     //結果を書き込む
     fprintf(fp_out,T_dash);
-    
+
     //出力ファイルを閉じる
     fclose(fp_out);
-
     free(T_dash);
     free(T_in);
 
